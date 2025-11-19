9817f91f5586a972ebea7fc6227f564c213e94d7
eliotadn <86308985+eliotadn@users.noreply.github.com>
2025-11-19 15:21:20 +0100

    Update Clock.py

--- Clock.py
+++ Clock.py
@@ -1,29 +1,22 @@
-# === Imports et initialisation ===
 from machine import Pin, I2C
 import network, ntptime, time
-
-# --- Config Wi-Fi ---
+# Wifi
 WIFI_SSID = "UREL-SC661-V-2.4G"
 WIFI_PSK  = "TomFryza"
-TZ_OFFSET_H = 1  # 1 hiver / 2 été
-
-# --- Config LED (GPIO2) ---
+TZ_OFFSET_H = 1
+#Config LED 
 led = Pin(2, Pin.OUT)
-
 def blink_led(duration_s=5, interval=0.2):
-    """Fait clignoter la LED pendant duration_s secondes."""
     end = time.ticks_ms() + int(duration_s * 1000)
     while time.ticks_ms() < end:
         led.on();  time.sleep(interval)
         led.off(); time.sleep(interval)
-
-# --- Paramètres d’alarme ---
-ALARM_HOUR = 14
-ALARM_MIN  = 46
-ALARM_SECOND = 50
+# Alarme parameter
+ALARM_HOUR = 15
+ALARM_MIN  = 18
+ALARM_SECOND = 00
 ALARM_DURATION = 10
 
-# --- Fonctions NTP / heure locale ---
 def wifi_connect(timeout=20):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
@@ -49,7 +42,7 @@ def local_time_hms():
     lt = time.localtime(t)
     return lt[3], lt[4], lt[5]
 
-# --- Programme principal ---
+#main
 wifi_connect()
 sync_ntp()
 
@@ -62,12 +55,10 @@ try:
         h, m, s = local_time_hms()
         print(f"{h:02d}:{m:02d}:{s:02d}", end="\r")
 
-        # Détection de l’heure d’alarme
         if h == ALARM_HOUR and m == ALARM_MIN and s == ALARM_SECOND and not triggered:
             blink_led(ALARM_DURATION)
             triggered = True
 
-        # Réarmer après la minute d’alarme
         if h != ALARM_HOUR or m != ALARM_MIN or s != ALARM_SECOND:
             triggered = False
 
