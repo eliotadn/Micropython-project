578937670facbec8a9326410b3265baca9e53261
Matthieu <38507131+Matthieu681@users.noreply.github.com>
2025-11-26 16:28:48 +0100

    Create air_sensor.py

---
+++ air_sensor.py
@@ -0,0 +1,19 @@
+from machine import ADC, Pin
+
+def init_sensor(pin_num):
+    adc = ADC(Pin(pin_num))
+    adc.atten(ADC.ATTN_11DB) # Full range 3.3V
+    adc.width(ADC.WIDTH_12BIT)
+    return adc
+
+def read_air_quality(adc):
+    raw = adc.read()
+    
+    # Basic Thresholds for MQ-5
+    quality = "Good"
+    if raw > 1500:
+        quality = "Moderate"
+    if raw > 2500:
+        quality = "Bad"
+        
+    return raw, quality
